6c206146325bb814229092a46295d62bf81b706b
Yonatan <yonatanfilar@gmail.com>
2024-04-20 00:27:56 +0300

    Add user controller, app.js, config

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+require('dotenv').config()
+const app = express()
+const ejs = require('ejs'); 
+const router = require("./routes/router.js")
+const { sessionMiddleware, excludeFromAuth } = require('./middleware/utils');
+
+
+app.use(sessionMiddleware);
+app.use(excludeFromAuth(['/signup', '/login']));
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(router)
+
+app.listen(3000, () => {
+     console.log('running on 3000')
+})
+
+module.exports = {
+    app
+}
